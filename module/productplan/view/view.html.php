dc02b91db9ab76367974010fcbf16b4eb7ac9888
shenpengfei <rg20china@gmail.com>
2015-10-20 13:55:09 +0800

    计划页面中，修复描述显示。

--- module/productplan/view/view.html.php
+++ module/productplan/view/view.html.php
@@ -80,7 +80,7 @@ js::import($webRoot .'js/ganttLine.js');
     <td>
       <fieldset>
         <legend><?php echo $lang->productplan->desc;?></legend>
-        <div class='content'><?php echo $product->desc;?></div>
+        <div class='content'><?php echo $plan->desc;?></div>
       </fieldset>
       <?php include '../../common/view/action.html.php';?>
       <div class='a-center f-16px strong'>
