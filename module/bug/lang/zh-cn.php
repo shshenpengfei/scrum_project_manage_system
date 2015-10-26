8372f34e32ff36730fab30575b90c06d5c9e10de
shenpengfei <rg20china@gmail.com>
2015-10-26 19:21:23 +0800

    增加了bug类型

--- module/bug/lang/zh-cn.php
+++ module/bug/lang/zh-cn.php
@@ -237,8 +237,13 @@ $lang->bug->browserList['uc']       = 'UC';
 $lang->bug->browserList['other']    = '其他';
 
 $lang->bug->typeList['']             = '';
-$lang->bug->typeList['codeerror']    = '代码错误';
 $lang->bug->typeList['interface']    = '界面优化';
+$lang->bug->typeList['codeerror']    = '代码错误';
+$lang->bug->typeList['standard']     = '标准规范';
+$lang->bug->typeList['compatible_devices']     = '设备兼容';
+$lang->bug->typeList['product_definition']     = '产品定义';
+$lang->bug->typeList['logical_problem']     = '逻辑问题';
+$lang->bug->typeList['ui']     = '界面显示';
 $lang->bug->typeList['designchange'] = '设计变更';
 $lang->bug->typeList['newfeature']   = '新增需求';
 $lang->bug->typeList['designdefect'] = '设计缺陷';
@@ -246,10 +251,10 @@ $lang->bug->typeList['config']       = '配置相关';
 $lang->bug->typeList['install']      = '安装部署';
 $lang->bug->typeList['security']     = '安全相关';
 $lang->bug->typeList['performance']  = '性能问题';
-$lang->bug->typeList['standard']     = '标准规范';
 $lang->bug->typeList['automation']   = '测试脚本';
 $lang->bug->typeList['trackthings']  = '事务跟踪';
 $lang->bug->typeList['others']       = '其他';
+
 
 $lang->bug->statusList['']         = '';
 $lang->bug->statusList['active']   = '激活';
